cd452fa6b497c802eb1ee030e5cab8b74473db65
spice <493136304@qq.com>
2016-06-04 16:24:19 +0800

    first commit

---
+++ ZTChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZTChart
+//
+//  Created by 谢展图 on 16/6/4.
+//  Copyright © 2016年 spice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
